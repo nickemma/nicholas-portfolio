b387ff9a94a474ec0c16c8b453a8468d8e8ec52d
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-12-14 14:16:51 +0100

    modified footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,50 +1,11 @@
 import React from 'react';
 import classes from './footer.module.css';
-import Link from 'next/link';
-
-const NAV_LINKS = [
-  {
-    path: '/',
-    display: 'Home',
-  },
-  {
-    path: '#about',
-    display: 'About',
-  },
-  {
-    path: '#services',
-    display: 'Services',
-  },
-  {
-    path: '#portfolio',
-    display: 'Portfolio',
-  },
-  {
-    path: '#contact',
-    display: 'Contact',
-  },
-];
-
 const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
 
   return (
     <footer className={`${classes.footer}`}>
-      <div className={`${classes.hold}`}>
-        <div className={`${classes.info}`}>
-          {NAV_LINKS.map((items, index) => (
-            <li key={index}>
-              <Link href={items.path}>{items.display}</Link>
-            </li>
-          ))}
-        </div>
-        <div className={`${classes.credit}`}>
-          <h3>
-            created by <span>Techie</span>Emma
-          </h3>
-        </div>
-      </div>
       <div className={`${classes.copy}`}>
         <p>
           {year} - developed by <span>Techie</span> Emma | all right reserved

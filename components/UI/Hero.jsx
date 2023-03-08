3104e602c898abdf439e3dc4b7745871f646771e
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-03-08 16:51:58 +0100

    Made changes

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
             I&apos;M NICHOLAS <br />
             EMMANUEL
           </h3>
-          <h5>Software Developer</h5>
+          <h5>Software Engineer</h5>
           <p>
             I can help you build a product, feature, or website. Look through
             some of my work and experience! If you like what you see and have a
